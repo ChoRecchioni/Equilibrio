b5a5e0aa2a7fb407bbdbcaf78ff79339f2b1af74
ChoRecchioni <105133755+ChoRecchioni@users.noreply.github.com>
2022-05-07 19:52:42 -0400

    correccion

--- EQUILIBRIO/WEBFORMS/Inicio.aspx.cs
+++ EQUILIBRIO/WEBFORMS/Inicio.aspx.cs
@@ -11,8 +11,7 @@ namespace equilibrio.METODOS
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            #region [ Page Load ]
-            #endregion
+
         }
     }
 }
