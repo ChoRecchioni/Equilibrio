687549590ff1ff76015aff6125e07beb1f3f33b2
ChoRecchioni <105133755+ChoRecchioni@users.noreply.github.com>
2022-05-09 20:28:19 -0400

    css iniciar sesión

--- EQUILIBRIO/WEBFORMS/IniciarSesión.aspx
+++ EQUILIBRIO/WEBFORMS/IniciarSesión.aspx
@@ -2,4 +2,10 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="IniciarSesion" id="background">
+        <asp:TextBox class="TxtBoxIS" ID="TxtUser" runat="server" Text="USUARIO"></asp:TextBox><br />
+        <asp:TextBox class="TxtBoxIS" ID="TxtPass" runat="server" Text="CONTRASEÑA"></asp:TextBox><br />
+        <asp:Button ID="BtnIniciar" runat="server" Text="INICIAR SESIÓN" /><br />
+        <asp:LinkButton ID="LnkRegistrar" runat="server">NO TIENE CUENTA? REGISTRESE</asp:LinkButton>
+    </div>
 </asp:Content>
--- EQUILIBRIO/WEBFORMS/IniciarSesión.aspx.designer.cs
+++ EQUILIBRIO/WEBFORMS/IniciarSesión.aspx.designer.cs
@@ -13,5 +13,41 @@ namespace equilibrio.WEBFORMS
 
     public partial class Formulario_web11
     {
+
+        /// <summary>
+        /// Control TxtUser.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtUser;
+
+        /// <summary>
+        /// Control TxtPass.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtPass;
+
+        /// <summary>
+        /// Control BtnIniciar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnIniciar;
+
+        /// <summary>
+        /// Control LnkRegistrar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton LnkRegistrar;
     }
 }
