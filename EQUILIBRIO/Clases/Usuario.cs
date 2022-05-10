f1e1587b68ecf763b607131bdf462e1c84d889f2
Daigo <diego.callpa@inacapmail.cl>
2022-05-10 13:49:38 -0400

    test

--- EQUILIBRIO/Clases/Usuario.cs
+++ EQUILIBRIO/Clases/Usuario.cs
@@ -3,17 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace EQUILIBRIO.Clases
+namespace equilibrio.Clases
 {
     public class Usuario
     {
-        private string codigo;
-        private string rut;
-        private string nombre;
-        private string apellido;
-        private string telefono;
-        private string pass;
-        private Direccion direccion;
-
     }
 }
