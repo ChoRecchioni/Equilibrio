de591e00585bdeb4eedca48f8ab8ed3a743539bb
ChoRecchioni <105133755+ChoRecchioni@users.noreply.github.com>
2022-05-21 00:44:11 -0400

    webform ver reseña

--- EQUILIBRIO/WEBFORMS CLI/VerReseña.aspx.cs
+++ EQUILIBRIO/WEBFORMS CLI/VerReseña.aspx.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using equilibrio.Clases;
 using equilibrio.Controller;
@@ -13,7 +14,7 @@ namespace equilibrio.WEBFORMS_CLI
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            SedeController.CargarSedes();
+            CargarDropSedes();
             CargarGrid();
         }
 
@@ -27,9 +28,69 @@ namespace equilibrio.WEBFORMS_CLI
                                        Puntuacion = re.Puntuacion,
                                        Comentario = re.Comentario
                                    };
-            GrdReseña.DataSource = listado.ToList();
 
-            GrdReseña.DataBind();
+            foreach (var item in listado)
+            {
+                //Se crear un objeto DIV dinamico por cada reseña
+                HtmlGenericControl divResena = new HtmlGenericControl("div");
+
+                divResena.Attributes.Add("class", "Reseña");
+
+                ///Genero la etiqueta titulo
+                //Label labelTitulo = new Label();
+                //labelTitulo.Text = item.Puntuacion.ToString();
+                //labelTitulo.CssClass = "LblTitulo";
+                //divResena.Controls.Add(labelTitulo);
+
+                for (int i = 0 ; i < item.Puntuacion ; i++)
+                {
+                    //Genero Imagen de estrella
+                    Image imgTitulo = new Image();
+                    imgTitulo.Height = 20;
+                    imgTitulo.Width = 20;
+                    imgTitulo.Style.Add("vertical-align", "bottom");
+                    imgTitulo.ImageUrl = "~/IMG/full-star.png";
+                    
+                    divResena.Controls.Add(imgTitulo);
+                }
+
+                //Genero Label tirulo Comentario
+                Label labelTituloComentario = new Label();
+                labelTituloComentario.CssClass = "LblTitulo";
+                labelTituloComentario.Text = "  - COMENTARIO:";
+
+                //Genero Label de comenario
+                Label labelComentario = new Label();
+                labelComentario.CssClass = "Lbl";
+                labelComentario.Text = item.Comentario;
+
+                //Agrego los objetos al nuevo DIV
+                
+                
+                divResena.Controls.Add(labelTituloComentario);
+                divResena.Controls.Add(labelComentario);
+
+                //Luego agrego el nuevo div con 1 comentario al DIV principal
+                DivResenas.Controls.Add(divResena);
+            }
+        }
+
+
+
+        public void CargarDropSedes()
+        {
+            SedeController.CargarSedes();
+
+            FiltroLocal.DataSource = from sed in SedeController.FindAll()
+                                     select new
+                                     {
+                                         codigo = sed.Codigo,
+                                         texto = sed.Nombre,
+                                     };
+            FiltroLocal.DataValueField = "codigo";
+            FiltroLocal.DataTextField = "texto";
+
+            FiltroLocal.DataBind();
         }
     }
 }
--- equilibrio/WEBFORMS CLI/VerReseña.aspx.designer.cs
+++ equilibrio/WEBFORMS CLI/VerReseña.aspx.designer.cs
@@ -33,40 +33,13 @@ namespace equilibrio.WEBFORMS_CLI
         protected global::System.Web.UI.WebControls.DropDownList FiltroLocal;
 
         /// <summary>
-        /// Control Ptaje.
+        /// Control DivResenas.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Ptaje;
-
-        /// <summary>
-        /// Control Image1.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Image Image1;
-
-        /// <summary>
-        /// Control Label3.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label3;
-
-        /// <summary>
-        /// Control Coment.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Coment;
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl DivResenas;
 
         /// <summary>
         /// Control GrdReseña.
