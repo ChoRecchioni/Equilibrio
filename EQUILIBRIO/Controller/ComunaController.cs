e4000c0293fce4ce299eecb0bcd7dd921e9bbcf3
Daigo <diego.callpa@inacapmail.cl>
2022-07-02 20:06:28 -0400

    nuevos controller

--- EQUILIBRIO/Controller/ComunaController.cs
+++ EQUILIBRIO/Controller/ComunaController.cs
@@ -9,6 +9,80 @@ namespace equilibrio.Controller
     public class ComunaController
     {
 
+        // Comienzo de nuevo Controller
+
+
+        //instancia de las entidades de la BD
+        static EQEntidades entidades = new EQEntidades();
+
+        //Metodo para listar todos
+        public static List<Comuna> findAll()
+        {
+
+            var com = from c in entidades.Comuna
+                      select c;
+
+            return com.ToList();
+        }
+
+        //Metodo para agregar
+        public static string AddComuna(string codigo, string nombre)
+        {
+
+            Comuna c = new Comuna()
+            {
+                codigo = int.Parse(codigo),
+                nombre = nombre,
+            };
+
+            entidades.Comuna.Add(c);
+            entidades.SaveChanges();
+
+            return "Revista agregada.";
+        }
+
+
+        //Metodo para encontrar uno
+        public static Comuna FindComuna(int codigo)
+        {
+
+            return entidades.Comuna.SingleOrDefault(c => c.codigo == codigo);
+        }
+
+
+        //Metodo para editar
+        public static string editComuna(string codigo, string nombre)
+        {
+            int intCod = int.Parse(codigo);
+            //revista r = entidades.revista.Find(id);//Busca por clave primaria
+            Comuna c = FindComuna(intCod);
+
+            if (c != null)
+            {
+                c.nombre = nombre;
+                entidades.SaveChanges();
+                return "Comuna modificada";
+            }
+            else
+            {
+                return "No se pudo modificar la comuna";
+            }
+        }
+
+
+        //Metodo para eliminar
+        public static string removeComuna(int codigo)
+        {
+
+            Comuna c = FindComuna(codigo);
+            entidades.Comuna.Remove(c);
+            entidades.SaveChanges();
+            return "Comuna eliminada";
+        }
+
+        // Fin nuevo Controller
+
+
         private static List<Comuna> listaComuna = new List<Comuna>();
 
         //Metodo para agregar Comuna
--- EQUILIBRIO/Controller/RegionController.cs
+++ EQUILIBRIO/Controller/RegionController.cs
@@ -8,6 +8,79 @@ namespace equilibrio.Controller
 {
     public class RegionController
     {
+
+        // Comienzo de nuevo Controller
+
+
+        //instancia de las entidades de la BD
+        static EQEntidades entidades = new EQEntidades();
+
+        //Metodo para listar todos
+        public static List<Region> findAll()
+        {
+
+            var reg = from r in entidades.Region
+                      select r;
+
+            return reg.ToList();
+        }
+
+        //Metodo para agregar
+        public static string AddRegion(string codigo, string nombre)
+        {
+
+            Region r = new Region()
+            {
+                codigo = int.Parse(codigo),
+                nombre = nombre,
+            };
+
+            entidades.Region.Add(r);
+            entidades.SaveChanges();
+
+            return "Región agregada.";
+        }
+
+
+        //Metodo para encontrar uno
+        public static Region FindRegion(int codigo)
+        {
+
+            return entidades.Region.SingleOrDefault(r => r.codigo == codigo);
+        }
+
+
+        //Metodo para editar
+        public static string editRegion(string codigo, string nombre)
+        {
+            int intCod = int.Parse(codigo);
+            //revista r = entidades.revista.Find(id);//Busca por clave primaria
+            Region r = FindRegion(intCod);
+
+            if (r != null)
+            {
+                r.nombre = nombre;
+                entidades.SaveChanges();
+                return "Region modificada";
+            }
+            else
+            {
+                return "No se pudo modificar la region";
+            }
+        }
+
+
+        //Metodo para eliminar
+        public static string removeRegion(int codigo)
+        {
+
+            Region r = FindRegion(codigo);
+            entidades.Region.Remove(r);
+            entidades.SaveChanges();
+            return "Region eliminada";
+        }
+
+        // Fin nuevo Controller
         private static List<Region> listaRegion = new List<Region>();
 
         //Metodo de registrar region
